62acab793c28afa777c1fc9df39bf6a470a6bb05
Marc Rousavy <marcrousavy@hotmail.com>
2021-09-19 16:04:45 +0200

    Fix expo-system-ui

--- packages/expo-system-ui/android/src/main/java/expo/modules/systemui/SystemUIModule.kt
+++ packages/expo-system-ui/android/src/main/java/expo/modules/systemui/SystemUIModule.kt
@@ -1,15 +1,20 @@
 package expo.modules.systemui
 
+import android.app.Activity
 import android.content.Context
-
-import org.unimodules.core.ExportedModule
-import org.unimodules.core.ModuleRegistry
-import org.unimodules.core.Promise
-import org.unimodules.core.interfaces.ExpoMethod
+import android.graphics.Color
+import android.view.WindowManager
+import expo.modules.core.ExportedModule
+import expo.modules.core.ModuleRegistry
+import expo.modules.core.interfaces.ExpoMethod
 
 class SystemUIModule(context: Context) : ExportedModule(context) {
 
   private var mModuleRegistry: ModuleRegistry? = null
+  private val activity: Activity
+    get() {
+      return context as? Activity ?: throw Error("Cannot get Main Activity!")
+    }
 
   override fun getName(): String {
     return NAME
@@ -21,20 +26,22 @@ class SystemUIModule(context: Context) : ExportedModule(context) {
 
   @ExpoMethod
   fun setNavigationBarColor(color: String) {
-    if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-      getWindow().setNavigationBarColor(/* TODO: Color */);
-    }
+    activity.window.navigationBarColor = Color.parseColor(color)
   }
 
   @ExpoMethod
-  fun setSystemUiVisibility(visibility: String) {
-    if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-      // TODO: Implement visibility with WindowInsets
+  fun setSystemUiVisibility(isVisible: Boolean) {
+    if (isVisible) {
+      activity.window.addFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
+      activity.window.clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    } else {
+      activity.window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
+      activity.window.clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN)
     }
   }
 
   companion object {
-    private val NAME = "ExpoSystemUI"
+    private const val NAME = "ExpoSystemUI"
     private val TAG = SystemUIModule::class.qualifiedName
   }
 }
