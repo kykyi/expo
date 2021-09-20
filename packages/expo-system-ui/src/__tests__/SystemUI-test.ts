2f8f206d36f42fe8cb28df6fe72f86d0167c9159
Marc Rousavy <marcrousavy@hotmail.com>
2021-09-20 10:08:38 +0200

    Fix SystemUI-test

--- packages/expo-system-ui/src/__tests__/SystemUI-test.ts
+++ packages/expo-system-ui/src/__tests__/SystemUI-test.ts
@@ -1,7 +1,10 @@
-import * as SystemUI from '../SystemUI';
+import { SystemUI } from '../SystemUI';
 
 describe('SystemUI', () => {
   it('setNavigationBarColor is defined', async () => {
     expect(SystemUI.setNavigationBarColor).toBeDefined();
+  });
+  it('setNavigationBarVisibility is defined', async () => {
+    expect(SystemUI.setNavigationBarVisibility).toBeDefined();
   });
 });
